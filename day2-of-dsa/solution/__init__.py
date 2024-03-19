5c6b05472d9ed87787cebf5c5b800d641a54b9d4
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-03-19 18:04:02 +0530

    assignment 1 done

--- day2-of-dsa/solution/__init__.py
+++ day2-of-dsa/solution/__init__.py
@@ -1,4 +1,5 @@
 from solution.first_question_sol import sortedarray
 from solution.second_question.solution import remove_non_int
 from solution.third_question_sol import calculate_avg
+from solution.fourth_ques_sol import isprime
 from solution.fifth_ques_sol import fabn as fabnicco_Series
